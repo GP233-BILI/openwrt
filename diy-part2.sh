f1c9c60ac17dba1de238cc0f3aa304108dbe9225
GP233-BILI <55877383+GP233-BILI@users.noreply.github.com>
2022-07-30 16:33:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,9 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+rm -rf package/lean/luci-theme-argon
+sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon  #argon-主题
